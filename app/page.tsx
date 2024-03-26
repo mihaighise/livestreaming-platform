84c37b13398636aaa45b699b30e234fdc8fd5a32
Mihai Ghise <mihaighise1@gmail.com>
2024-03-26 14:16:44 +0200

    wip authentication using Clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return (
-    <Button size="lg" variant="custom">
-      Click me
-    </Button>
-  );
+  return <p>Only authenticated users can see this</p>;
 }
